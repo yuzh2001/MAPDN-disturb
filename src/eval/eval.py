647fa2843f08ab1efd381fb0433f244c9edc2efb
yuzh <yuzhuohao2023@iscas.ac.cn>
2025-04-29 11:08:08 +0800

    feat: 整理eval

--- src/eval/eval.py
+++ src/eval/eval.py
@@ -1,6 +1,5 @@
 import torch
 import yaml
-import pickle
 from mapdn.models.model_registry import Model, Strategy
 from mapdn.environments.var_voltage_control.voltage_control_env import VoltageControl
 from mapdn.utilities.util import convert
@@ -25,68 +24,26 @@ def run(configs: EvalHydraEntryConfig):
     # for disturbance in configs.disturbances:
     # OmegaConf.structured(DisturbanceConfig(**disturbance))
 
-    # 2. 运行
+    # 2. 读取参数
     rich.print(OmegaConf.to_container(configs, resolve=True))
     argv = configs.eval_config
     global_prefix = "./mapdn"
 
-    # load env args
-    with open(f"{global_prefix}/args/env_args/{argv.env}.yaml", "r") as f:
-        env_config_dict = yaml.safe_load(f)["env_args"]
-    data_path = env_config_dict["data_path"].split("/")
-    data_path[-1] = argv.scenario
-    env_config_dict["data_path"] = "/".join(data_path)
-
-    # set the action range
-    net_topology = argv.scenario
-    if argv.scenario == "case33_3min_final":
-        env_config_dict["action_bias"] = 0.0
-        env_config_dict["action_scale"] = 0.8
-    elif argv.scenario == "case141_3min_final":
-        env_config_dict["action_bias"] = 0.0
-        env_config_dict["action_scale"] = 0.6
-    elif argv.scenario == "case322_3min_final":
-        env_config_dict["action_bias"] = 0.0
-        env_config_dict["action_scale"] = 0.8
-
-    # 3. 设置环境参数
-    assert argv.mode in [
-        "distributed",
-        "decentralised",
-    ], "Please input the correct mode, e.g. distributed or decentralised."
-    env_config_dict["mode"] = argv.mode
-    env_config_dict["voltage_barrier_type"] = argv.voltage_barrier_type
-
-    # for one-day test
-    env_config_dict["episode_limit"] = 480
-
-    # load default args
-    with open(global_prefix + "/args/default.yaml", "r") as f:
-        default_config_dict = yaml.safe_load(f)
-    default_config_dict["max_steps"] = 480
-
-    # load alg args
-    with open(global_prefix + "/args/alg_args/" + argv.alg + ".yaml", "r") as f:
-        alg_config_dict = yaml.safe_load(f)["alg_args"]
-        alg_config_dict["action_scale"] = env_config_dict["action_scale"]
-        alg_config_dict["action_bias"] = env_config_dict["action_bias"]
-
     log_name = "-".join(
         [
             # argv.env,
-            net_topology,
+            argv.scenario,
             # argv.mode,
             argv.alg,
             argv.voltage_barrier_type,
             argv.alias,
         ]
     )
-
     date_stamp = datetime.now().strftime("%m%d-%H%M")
     wandb_name = "-".join(
         [
             f"[{date_stamp}]",
-            # net_topology,
+            # argv.scenario,
             # argv.mode,
             argv.alg,
             # argv.voltage_barrier_type,
@@ -94,52 +51,6 @@ def run(configs: EvalHydraEntryConfig):
         ]
     )
     print(f"Now testing: {wandb_name}")
-    alg_config_dict = {**default_config_dict, **alg_config_dict}
-
-    # define envs
-    if configs.disturbances:
-        env = VoltageControl(env_config_dict, configs.disturbances)
-    else:
-        env = VoltageControl(env_config_dict)
-
-    alg_config_dict["agent_num"] = env.get_num_of_agents()
-    alg_config_dict["obs_size"] = env.get_obs_size()
-    alg_config_dict["action_dim"] = env.get_total_actions()
-    alg_config_dict["cuda"] = True
-    args = convert(alg_config_dict)
-
-    # 读取模型训练结果
-    if argv.save_path[-1] == "/":
-        save_path = argv.save_path
-    else:
-        save_path = argv.save_path + "/"
-
-    LOAD_PATH = save_path + f"{configs.save_group}/" + log_name + "/model.pt"
-    print(f"Loading model from {LOAD_PATH}")
-
-    model = Model[argv.alg]
-
-    strategy = Strategy[argv.alg]
-
-    if args.target:
-        target_net = model(args)
-        behaviour_net = model(args, target_net)
-    else:
-        behaviour_net = model(args)
-    checkpoint = (
-        torch.load(LOAD_PATH, map_location="cpu")
-        if not args.cuda
-        else torch.load(LOAD_PATH)
-    )
-    behaviour_net.load_state_dict(checkpoint["model_state_dict"])
-
-    if strategy == "pg":
-        test = PGTester(args, behaviour_net, env, argv.render)
-    elif strategy == "q":
-        raise NotImplementedError("This needs to be implemented.")
-    else:
-        raise RuntimeError("Please input the correct strategy, e.g. pg or q.")
-
     runa = wandb.init(
         project="mapdn",
         name=wandb_name,
@@ -151,18 +62,99 @@ def run(configs: EvalHydraEntryConfig):
     )
     wandb.define_metric("terminate_cnt", summary="min")
 
+    # 3. ENV的参数
+    def _read_env_args():
+        with open(f"{global_prefix}/args/env_args/{argv.env}.yaml", "r") as f:
+            env_config_dict = yaml.safe_load(f)["env_args"]
+        data_path = env_config_dict["data_path"].split("/")
+        data_path[-1] = argv.scenario
+        env_config_dict["data_path"] = "/".join(data_path)
+
+        # set the action range
+        if argv.scenario == "case33_3min_final":
+            env_config_dict["action_bias"] = 0.0
+            env_config_dict["action_scale"] = 0.8
+        elif argv.scenario == "case141_3min_final":
+            env_config_dict["action_bias"] = 0.0
+            env_config_dict["action_scale"] = 0.6
+        elif argv.scenario == "case322_3min_final":
+            env_config_dict["action_bias"] = 0.0
+            env_config_dict["action_scale"] = 0.8
+
+        assert argv.mode in [
+            "distributed",
+            "decentralised",
+        ], "Please input the correct mode, e.g. distributed or decentralised."
+        env_config_dict["mode"] = argv.mode
+        env_config_dict["voltage_barrier_type"] = argv.voltage_barrier_type
+        env_config_dict["episode_limit"] = 480  # for one-day test
+        return env_config_dict
+
+    env_config_dict = _read_env_args()
+
+    # 5. 实例化ENV
+    if not configs.disturbances:
+        configs.disturbances = []
+
+    env = VoltageControl(env_config_dict, configs.disturbances)
+
+    # 4. 算法的参数
+    def _read_general_args():
+        with open(global_prefix + "/args/default.yaml", "r") as f:
+            default_config_dict = yaml.safe_load(f)
+        default_config_dict["max_steps"] = 480
+        return default_config_dict
+
+    default_config_dict = _read_general_args()
+
+    def _read_alg_args():
+        with open(global_prefix + "/args/alg_args/" + argv.alg + ".yaml", "r") as f:
+            alg_config_dict = yaml.safe_load(f)["alg_args"]
+        alg_config_dict["action_scale"] = env_config_dict["action_scale"]
+        alg_config_dict["action_bias"] = env_config_dict["action_bias"]
+
+        alg_config_dict = {**default_config_dict, **alg_config_dict}
+        alg_config_dict["agent_num"] = env.get_num_of_agents()
+        alg_config_dict["obs_size"] = env.get_obs_size()
+        alg_config_dict["action_dim"] = env.get_total_actions()
+        alg_config_dict["cuda"] = True
+
+        return alg_config_dict
+
+    alg_config_dict = _read_alg_args()
+
+    args = convert(alg_config_dict)
+
+    model = Model[argv.alg]
+
+    if args.target:
+        target_net = model(args)
+        behaviour_net = model(args, target_net)
+    else:
+        behaviour_net = model(args)
+
+    # 6. 读取模型checkpoint
+    if argv.save_path[-1] == "/":
+        save_path = argv.save_path
+    else:
+        save_path = argv.save_path + "/"
+
+    LOAD_PATH = save_path + f"{configs.save_group}/" + log_name + "/model.pt"
+    print(f"Loading model from {LOAD_PATH}")
+
+    checkpoint = (
+        torch.load(LOAD_PATH, map_location="cpu")
+        if not args.cuda
+        else torch.load(LOAD_PATH)
+    )
+    behaviour_net.load_state_dict(checkpoint["model_state_dict"])
+
+    strategy = Strategy[argv.alg]
+    assert strategy == "pg"
+    test = PGTester(args, behaviour_net, env, argv.render)
+
     if argv.test_mode == "single":
-        # record = test.run(199, 23, 2) # (day, hour, 3min)
-        # record = test.run(730, 23, 2) # (day, hour, 3min)
         record = test.run(argv.test_day, 23, 2)
-        with open(
-            "reproduction/eval/test_record_"
-            + log_name
-            + f"_day{argv.test_day}"
-            + ".pickle",
-            "wb",
-        ) as f:
-            pickle.dump(record, f, pickle.HIGHEST_PROTOCOL)
     elif argv.test_mode == "batch":
         record = test.batch_run(argv.eval_episodes)
         data = [v for v in record.values()]
@@ -175,15 +167,6 @@ def run(configs: EvalHydraEntryConfig):
             }
         )
         runa.finish()
-        with open(
-            "reproduction/eval/test_record_"
-            + log_name
-            + "_"
-            + argv.test_mode
-            + ".pickle",
-            "wb",
-        ) as f:
-            pickle.dump(record, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
